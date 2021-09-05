236ef0eae0cc3b4301bd365f4af35ebfe3796584
Anton Bobryshev <65329095+tosls@users.noreply.github.com>
2021-09-06 01:40:47 +0700

    Create the Row View

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Антон Бобрышев on 06.09.2021.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
