61bc2ea9be92ef3fa68e4806eba2c5a8cdd78f44
Anton Bobryshev <65329095+tosls@users.noreply.github.com>
2021-09-05 20:11:33 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Антон Бобрышев on 05.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
